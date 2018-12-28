c8c93c9ac7811beef7071430bb9610e8426339e7
StriveMcally <37031463+StriveMcally@users.noreply.github.com>
2018-12-28 14:12:13 +0800

    全部提交

---
+++ Mcally.UI.Web/App_Start/BundleConfig.cs
@@ -0,0 +1,44 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace Mcally.UI.Web
+{
+    public class BundleConfig
+    {
+        // 有关捆绑的详细信息，请访问 https://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                     "~/Content/css/bootstrap.min.css",
+                     "~/Content/css/style.css",
+                     "~/Content/css/login.min.css",
+                     "~/Content/ruoyi/css/ry-ui.css",
+                     "~/Content/css/font-awesome.min.css"
+                  
+                    ));
+        
+            bundles.Add(new ScriptBundle("~/bundles/Scripts1").Include(
+                        "~/Scripts/js/bootstrap.min.js",
+                        "~/Scripts/ajax/libs/validate/jquery.validate.min.js",
+                        "~/Scripts/ajax/libs/layer/layer.min.js",
+                        "~/Scripts/ajax/libs/validate/messages_zh.min.js",
+                        "~/Scripts/ajax/libs/blockUI/jquery.blockUI.js",
+                        "~/Scripts/ruoyi/js/ry-ui.js",
+                        "~/Scripts/ruoyi/login.js",
+                         "~/Scripts/ajax/libs/iCheck/icheck.min.js"
+
+                        ));
+
+            bundles.Add(new ScriptBundle("~/bundles/Scripts").Include(
+                      "~/Scripts/js/jquery.min.js"
+                     
+
+                      ));
+
+
+
+
+        }
+
+    }
+}
---
+++ Mcally.UI.Web/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Mcally.UI.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+       
+
+        public ActionResult login()
+        {
+           
+            return View();
+        }
+
+        public ActionResult index1()
+        {
+
+            return View();
+        }
+        public ActionResult index2()
+        {
+
+            return View();
+        }
+
+        public ActionResult index3()
+        {
+
+            return View();
+        }
+
+        public ActionResult index4()
+        {
+
+            return View();
+        }
+
+
+        public ActionResult Welcome()
+        {
+
+            return View();
+        }
+
+    }
+}
