5a2d16e5cda35256b598e68a8c88b00c12dc14cc
StriveMcally <37031463+StriveMcally@users.noreply.github.com>
2018-12-29 14:21:28 +0800

    模块化分离

---
+++ HTMLUI/HTMLUIAreaVirtualPathRegistration.cs
@@ -0,0 +1,27 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HTMLUI
+{
+    public class MESAreaVirtualPathRegistration : IAreaVirtualPathRegistration
+    {
+        public List<KeyValuePair<string, string>> GetPath()
+        {
+            var pathList = new List<KeyValuePair<string, string>>();
+            pathList.Add(new KeyValuePair<string, string>("HTMLUI", "HTMLUI"));
+
+            return pathList;
+        }
+    }
+
+    public interface IAreaVirtualPathRegistration
+    {
+        List<KeyValuePair<string, string>> GetPath();
+    }
+}
+
+
+
---
+++ HTMLUI/HTMLUIRegistration.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HTMLUI
+{
+    public class MESAreaRegistration : AreaRegistration
+    {
+        public override string AreaName
+        {
+            get
+            {
+                return "HTMLUI";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            context.MapRoute(
+                "MES_default",
+                "HTMLUI/{controller}/{action}/{id}",
+                new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+                namespaces: new string[] { "Mcally.UI.Web.Controllers" }
+            );
+        }
+    }
+}
